a05556d51740b6e820290091ed8365355cd889f4
kamlesh <katrekamlesh7@gmail.com>
2021-06-05 23:38:58 +0530

    Added Reverse Star Pattern.py

---
+++ Codechef/DSA learning series/Easy Problems to Get Started/Reverse Star Pattern.py
@@ -0,0 +1,33 @@
+# You're given a number N. Print the first N lines of the below-given pattern.
+#
+#     *
+#    **
+#   ***
+#  ****
+# *****
+# Input:
+# First-line will contain the number N.
+# Output:
+# Print the first N lines of the given pattern.
+#
+# Constraints
+# 1≤N≤200
+# Sample Input 1:
+# 4
+# Sample Output 1:
+#    *
+#   **
+#  ***
+# ****
+# Sample Input 2:
+# 2
+# Sample Output 2:
+#  *
+# **
+# EXPLANATION:
+# In the first example, we'll print the first 4 lines of the given pattern.
+# In the second example, we'll print the first 2 lines of the given pattern
+
+num=int(input())
+for i in range(num):
+    print(("*"*i).rjust(num," "))
