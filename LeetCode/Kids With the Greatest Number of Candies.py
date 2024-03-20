6028a9d0ec70f4a69677b0f346ec2ce2556f7da7
katre <katrekamlesh7@gmail.com>
2024-03-21 04:30:59 +0530

    added Kids With the Greatest Number of Candies.py

--- LeetCode/Kids With the Greatest Number of Candies.py
+++ LeetCode/Kids With the Greatest Number of Candies.py
@@ -1,6 +1,5 @@
 def kidsWithCandies(candies, extraCandies):
     max_candies=max(candies)
-    print(max_candies)
     return [x+extraCandies>=max_candies for x in candies ]
 if __name__ == '__main__':
     print(kidsWithCandies([2, 3, 5, 1, 3], 3))
