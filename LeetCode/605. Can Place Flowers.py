a6f4fec493c2d0048e96f39525f8c0df4d43dbd7
katre <katrekamlesh7@gmail.com>
2024-03-24 21:34:02 +0530

    added 605. Can Place Flowers.py optimised one

--- LeetCode/605. Can Place Flowers.py
+++ LeetCode/605. Can Place Flowers.py
@@ -6,42 +6,54 @@ class Solution(object):
         :rtype: bool
         """
         zero_count = 0
-        if len(flowerbed) == 1:
-            if flowerbed[0] == 0:
+        fl = [0] + flowerbed + [0]
+        for x in range(1, len(fl) - 1):
+            if fl[x - 1] == 0 and fl[x] == 0 and fl[x + 1] == 0:
                 zero_count = zero_count + 1
+                fl[x] = 1
             else:
                 pass
-        elif len(flowerbed) == 2:
-            if flowerbed[0] == 0 and flowerbed[1] == 0:
-                zero_count = zero_count + 1
-            else:
-                pass
-        else:
-            if flowerbed[0] ==0 and flowerbed[1]==0:
-                zero_count=zero_count+1
-                flowerbed[0]=1
-            for x in range(1,len(flowerbed)-1):
+            if zero_count >= n:
+                return True
+        return False
 
-                print(x)
-                preele = flowerbed[x - 1]
-                curele = flowerbed[x]
-                nexele = flowerbed[x + 1]
-
-                if preele == 0 and curele == 0 and nexele == 0:
-                    zero_count = zero_count + 1
-                    flowerbed[x]=1
-                else:
-                    pass
-            if flowerbed[-1]==0 and flowerbed[-2]==0:
-                zero_count=zero_count+1
-                flowerbed[-1]=1
-            if zero_count == n:
-                return  True
-
-        if n <= zero_count:
-            return True
-        else:
-            return False
+        # zero_count = 0
+        # if len(flowerbed) == 1:
+        #     if flowerbed[0] == 0:
+        #         zero_count = zero_count + 1
+        #     else:
+        #         pass
+        # elif len(flowerbed) == 2:
+        #     if flowerbed[0] == 0 and flowerbed[1] == 0:
+        #         zero_count = zero_count + 1
+        #     else:
+        #         pass
+        # else:
+        #     if flowerbed[0] ==0 and flowerbed[1]==0:
+        #         zero_count=zero_count+1
+        #         flowerbed[0]=1
+        #     for x in range(1,len(flowerbed)-1):
+        #
+        #         print(x)
+        #         preele = flowerbed[x - 1]
+        #         curele = flowerbed[x]
+        #         nexele = flowerbed[x + 1]
+        #
+        #         if preele == 0 and curele == 0 and nexele == 0:
+        #             zero_count = zero_count + 1
+        #             flowerbed[x]=1
+        #         else:
+        #             pass
+        #     if flowerbed[-1]==0 and flowerbed[-2]==0:
+        #         zero_count=zero_count+1
+        #         flowerbed[-1]=1
+        #     if zero_count == n:
+        #         return  True
+        #
+        # if n <= zero_count:
+        #     return True
+        # else:
+        #     return False
 
 
 call = Solution()
